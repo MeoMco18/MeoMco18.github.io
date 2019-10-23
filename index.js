f9e9ebe467912c5f9acc566bc745b89bcf6a3a5f
Meo <09mesh12@gmail.com>
2019-10-23 12:27:34 +0100

    2nd Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+ console.log(5);
+    alert('Click OK to see my website.');
+    console.log('this is my website and I am learning how to use javascript properly.')
+    console.log('It is still a work in progress.')
+    var x = document.getElementsByClassName("Button");
+
+    function myFunction() {
+        console.log(document.getElementById("demo"));
+      document.getElementById("demo").innerHTML = "Ta-da."; 
+    }
